743a39b9a2c355623f05bb73d599f45e4ef33ef0
NJ Pearman <n.pearman@gmail.com>
2020-02-12 11:55:43 +0100

    Conditionally re-renders users#new on failed save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save!
-    session[:user_id] = user.id
-    redirect_to ideas_path
+    if @user.save
+      session[:user_id] = user.id
+      redirect_to ideas_path
+    else
+      render 'new'
+    end
   end
 
   private
