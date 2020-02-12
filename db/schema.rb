6b642ef8ba1a96fa88675b7e0102d5b1d8bc17e4
NJ Pearman <n.pearman@gmail.com>
2020-02-12 11:37:36 +0100

    Adds User#password_digest with has_secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200210154429) do
+ActiveRecord::Schema.define(version: 20200212103547) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20200210154429) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
