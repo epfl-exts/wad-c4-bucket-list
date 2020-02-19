88b0e0b41df2b8eb10a52f21c5efafa8fba97bc2
NJ Pearman <n.pearman@gmail.com>
2020-02-19 17:28:36 +0100

    Creates the account#goals action

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
   get 'account', to: 'account#edit'
   patch 'account', to: 'account#update'
   get 'account/ideas'
+  get 'account/goals'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
