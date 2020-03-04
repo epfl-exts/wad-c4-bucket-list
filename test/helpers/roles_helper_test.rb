6dc8af03c653b255fa442d0731c6e44c5919d8b2
NJ Pearman <n.pearman@gmail.com>
2020-03-04 11:13:31 +0100

    Tests that Idea owner and edit Idea

--- test/helpers/roles_helper_test.rb
+++ test/helpers/roles_helper_test.rb
@@ -2,4 +2,14 @@ require 'test_helper'
 
 class RolesHelperTest < ActionView::TestCase
   attr_accessor :current_user
+
+  test "owner can edit an Idea they own" do
+    owner = User.create! email: "owner@epfl.ch",
+      password: "password"
+
+    self.current_user = owner
+    idea = Idea.create! user: owner, title: "A fun idea"
+
+    assert can_edit?(idea)
+  end
 end
