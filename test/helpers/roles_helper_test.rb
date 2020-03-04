7fd929b641ca13aeff504033c79ffb7616df144a
NJ Pearman <n.pearman@gmail.com>
2020-03-04 11:16:53 +0100

    Tests that admin can edit Ideas they do not own

--- test/helpers/roles_helper_test.rb
+++ test/helpers/roles_helper_test.rb
@@ -12,4 +12,18 @@ class RolesHelperTest < ActionView::TestCase
 
     assert can_edit?(idea)
   end
+
+  test "admin can edit an Idea they do not own" do
+    admin = User.create! email: "admin@epfl.ch",
+      password: "password",
+      role: "admin"
+
+    self.current_user = admin
+
+    owner = User.create! email: "owner@epfl.ch",
+      password: "password"
+    idea = Idea.create! user: owner, title: "A fun idea"
+
+    assert can_edit?(idea)
+  end
 end
