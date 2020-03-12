03784e287fce586587b8e81c980b97d10e6242a4
NJ Pearman <n.pearman@gmail.com>
2020-03-12 16:29:37 +0100

    Allows goals#create to respond to HTML and JS

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -5,6 +5,9 @@ class GoalsController < ApplicationController
     user = current_user
     idea = Idea.find(params[:idea_id])
     user.goals << idea
-    redirect_to idea_path(idea)
+    respond_to do |format|
+      format.html { redirect_to idea_path(idea) }
+      format.js { render 'create' }
+    end
   end
 end
